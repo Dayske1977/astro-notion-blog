e7e645f45ce5c177563d17d4c5c52b901657b528
dayske1977 <personal@petite-poupee.com>
2024-03-23 01:16:10 +0900

    fix

--- src/server-constants.ts
+++ src/server-constants.ts
@@ -2,7 +2,7 @@ export const NOTION_API_SECRET =
   import.meta.env.NOTION_API_SECRET || process.env.NOTION_API_SECRET || ''
 export const DATABASE_ID =
   import.meta.env.DATABASE_ID || process.env.DATABASE_ID || ''
-  export const INDEX_PAGE_ID =
+export const INDEX_PAGE_ID =
   import.meta.env.INDEX_PAGE_ID || process.env.INDEX_PAGE_ID || ''
 
 export const CUSTOM_DOMAIN =
