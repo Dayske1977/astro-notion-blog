a7acb1560d43f7f11f109814f93d0c818bb08df8
dayske1977 <personal@petite-poupee.com>
2025-02-21 16:18:04 +0900

    20250221_2

--- src/lib/interfaces.ts
+++ src/lib/interfaces.ts
@@ -12,7 +12,6 @@ export interface Post {
   Cover: FileObject | null
   Slug: string
   Date: string
-  UpdateDate: string
   Tags: SelectProperty[]
   Excerpt: string
   FeaturedImage: FileObject | null
